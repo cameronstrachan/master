1ea6743d4fd5af30a617e3dea4256a52d09c61e1
CameronStrachan <strachc@gmail.com>
2018-12-12 15:53:19 +0100

    change in cores, wolf driver

--- wolf/driver.py
+++ wolf/driver.py
@@ -16,12 +16,12 @@ from modules.ctb_functions import *
 runqiime = input("\n" + "Run Qiime on data from Wetzels et al. 2018? (y or n):")
 
 if runqiime == 'y':
-	sg.runqiime(inputfolderloc='dataflow/01-fastq/wetzels2018', paired=True, numcores=7)
+	sg.runqiime(inputfolderloc='dataflow/01-fastq/wetzels2018', paired=True, numcores=24)
 
 ### RUN QIIME
 runqiime = input("\n" + "Run Qiime on data from Wu et al. 2017? (y or n):")
 
 if runqiime == 'y':
-	sg.runqiime(inputfolderloc='dataflow/01-fastq/wu2017', paired=False, numcores=7)
+	sg.runqiime(inputfolderloc='dataflow/01-fastq/wu2017', paired=False, numcores=24)
 	
 sg.runqiimemerge(file1folder='wetzels2018', file2folder='wu2018')
