80a82c5e3c4aba1b04a2991a96004a3746e8a6cd
CameronStrachan <strachc@gmail.com>
2019-01-03 11:52:05 +0100

    mod ldriver.py'

--- rumen/ldriver.py
+++ rumen/ldriver.py
@@ -4,7 +4,7 @@ import subprocess
 import pandas as pd
 
 # custom libraries
-sys.path.insert(0, '/Users/cameronstrachan/master/') 
+sys.path.insert(0, '/home/strachan/master/') 
 from modules import seq_core_lin as sc
 from modules import seq_gen_lin as sg
 
