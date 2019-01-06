e7129c1c89966b6b3996d9d0acae77f3b6738096
CameronStrachan <strachc@gmail.com>
2019-01-06 14:06:59 +0100

    small

--- rumen/src/python/blasttables2seqs.py
+++ rumen/src/python/blasttables2seqs.py
@@ -10,11 +10,21 @@ df = df.append(df2)
 df = df[(df['length'] == 190)]
 df = df[(df['pident'] >= 95)]
 
+unique_seqs = list()
+
 with open('dataflow/01-nucl/lacto_signal_differential_all_seqs_genomes.fasta', 'w') as file:
     for index, row in df.iterrows():
+        
+        unique_seq = row['sseqid']
+        print(unique_seq)
+        
+        if (unique_seq in unique_seqs) != True:
+            
+            header = ">" + row['Database'] + "_" + str(index)
+            file.write(header + "\n")
     
-        header = ">" + row['Database'] + "_" + str(index)
-        file.write(header + "\n")
-    
-        seq = row['sseq'].replace("-", "")
-        file.write(seq + "\n")
+            seq = row['sseq'].replace("-", "")
+            file.write(seq + "\n")
+        
+            unique_seqs.append(unique_seq)
+        
