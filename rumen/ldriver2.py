170d24518f70d58c0a1b185669128b50af571a5b
CameronStrachan <strachc@gmail.com>
2019-01-07 14:34:08 +0100

    add 16s extractor

--- rumen/ldriver2.py
+++ rumen/ldriver2.py
@@ -209,3 +209,23 @@ if makeheadermap == 'y':
 		df['file'] = file
 		
 		df.to_csv(headerfile + file.split('.fa')[0] + '.csv')
+
+runcommand = input("\n" + "Run FastTree? (y or n):")
+
+	if runcommand == 'y':
+
+		genomes_df = pd.read_csv('dataflow/00-meta/selected_genomes.csv', low_memory=False)
+		genomes = genomes_df['Genome'].tolist()
+		files = [item + "_rename.fasta" for item in genomes]
+
+		16s_seqs = list()
+
+		for file in files:
+			outputfile = file.split('.fa')[0] + "_16s.fasta"
+			command = '../../barrnap/bin/barrnap --threads 20 --kingdom bac -o dataflow/02-16s/' + outputfile + ' < ' + 'dataflow/01-nucl/' + file
+			os.system(command)
+			16s_seqs.append(outputfile)
+
+		sg.concat(inputfolder='dataflow/02-16s/', outputpath='dataflow/02-16s/lacto_signal_differential_seqs_genomes_16s.fasta', filenames=16s_seqs)
+
+
