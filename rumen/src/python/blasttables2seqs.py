047e490fc8fbdb00199a5d40304390d2e5cffb7b
CameronStrachan <strachc@gmail.com>
2019-01-08 11:41:52 +0100

    code update

--- rumen/src/python/blasttables2seqs.py
+++ rumen/src/python/blasttables2seqs.py
@@ -7,8 +7,8 @@ df2['Database'] = 'Rumen'
 
 df = df.append(df2)
 
-df = df[(df['length'] >= 185)]
-df = df[(df['pident'] >= 90)]
+df = df[(df['length'] == 190)]
+df = df[(df['pident'] >= 85)]
 
 unique_seqs = list()
 
