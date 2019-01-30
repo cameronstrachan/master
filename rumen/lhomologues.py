545058abb4c2fe559a925426836af1ed9b478911
CameronStrachan <strachc@gmail.com>
2019-01-30 17:36:22 +0100

    rbh R

--- rumen/lhomologues.py
+++ rumen/lhomologues.py
@@ -31,3 +31,21 @@ if runcommand == 'y':
 	file_obj.setOutputLocation('dataflow/01-prot/')
 	file_obj.subsetfasta(seqlist = genes, headertag='homologues')
 
+runcommand = input("\n" + "Create a nucl fasta with just the homologies? (y or n):")
+
+if runcommand == 'y':
+
+	genomes_df = pd.read_csv('dataflow/00-meta/checkM_summary_clean_prevotella.csv', low_memory=False)
+	genomes_df_rumen = genomes_df[genomes_df['source'] == 'rumen']
+	genomes = genomes_df_rumen['BinID'].tolist()
+	files = [item + "_rename.fasta" for item in genomes]
+
+	sg.concat(inputfolder='dataflow/01-prot/genes/', outputpath='dataflow/01-prot/genes/rumen_prevotella.fasta', filenames=files)
+
+	genes_df = pd.read_csv('dataflow/00-meta/df_conserved_homologues.csv', low_memory=False)
+	genes = genes_df['sseqid'].tolist()
+
+	file_obj = sc.Fasta('rumen_prevotella.fasta', 'dataflow/01-prot/genes/')
+	file_obj.setOutputName('rumen_prevotella_homologues.fasta')
+	file_obj.setOutputLocation('dataflow/01-prot/genes/')
+	file_obj.subsetfasta(seqlist = genes, headertag='homologues')
