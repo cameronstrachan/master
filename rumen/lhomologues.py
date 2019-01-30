456454a46453c8df3eff0c9a112bb6cbd213a96c
CameronStrachan <strachc@gmail.com>
2019-01-30 13:21:54 +0100

    rbh R

---
+++ rumen/lhomologues.py
@@ -0,0 +1,28 @@
+import os, sys
+import subprocess
+import pandas as pd
+
+# custom libraries
+sys.path.insert(0, '/home/strachan/master/') 
+from modules import seq_core_lin as sc
+from modules import seq_gen_lin as sg
+
+runcommand = input("\n" + "Run RBH analaysis to export shared homolgoues? (y or n):")
+
+if runcommand == 'y':
+	os.system("Rscript src/R/extract_RBH_sharedPI_highlyconserved.R")
+
+
+genomes_df = pd.read_csv('dataflow/00-meta/checkM_summary_clean_prevotella.csv', low_memory=False)
+genomes = genomes_df['BinID'].tolist()
+files = [item + ".fasta" for item in genomes]
+
+sg.concat(inputfolder='dataflow/01-prot/', outputpath='dataflow/01-prot/rumen_prevotella.fasta', filenames=files)
+
+genes_df = pd.read_csv('dataflow/00-meta/df_conserved_homologues.csv', low_memory=False)
+genes = genes_df['qseqid'].tolist()
+
+file_obj = sc.Fasta('rumen_prevotella.fasta', 'dataflow/01-prot/')
+file_obj.setOutputName('rumen_prevotella_homologues.fasta')
+file_obj.setOutputLocation('dataflow/01-prot/')
+file_obj.subsetfasta(seqlist = genes, headertag='homologues')
