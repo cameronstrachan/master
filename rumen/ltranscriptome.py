9923779a890f2f538033379ad8a48a2fbdeef8ab
CameronStrachan <strachc@gmail.com>
2019-01-31 13:23:23 +0100

    y

--- rumen/ltranscriptome.py
+++ rumen/ltranscriptome.py
@@ -9,4 +9,4 @@ from modules import seq_core_lin as sc
 from modules import seq_gen_lin as sg
 from modules import seq_scrape as ss
 
-srafastqdownlaod('SRX1585089', outputdir='dataflow/01-fastq/marre2017')
+ss.srafastqdownlaod('SRX1585089', outputdir='dataflow/01-fastq/marre2017')
