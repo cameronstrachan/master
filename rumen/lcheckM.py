17bc55e31ee134433316d0cc6d41fa13c5c2c8cf
CameronStrachan <strachc@gmail.com>
2019-02-04 10:41:26 +0100

    check taxon

--- rumen/lcheckM.py
+++ rumen/lcheckM.py
@@ -1,9 +1,9 @@
 import os, sys
 import subprocess
-import pandas as pd
+#import pandas as pd
 
 # custom libraries
-sys.path.insert(0, '/home/strachan/master/') 
+sys.path.insert(0, '/home/strachan/master/')
 from modules import seq_core_lin as sc
 from modules import seq_gen_lin as sg
 
@@ -16,8 +16,8 @@ from modules import seq_gen_lin as sg
 runcommand = input("\n" + "Run checkM pipeline on all rumen and prevotella genomes? (y or n):")
 
 if runcommand == 'y':
-	os.system("checkm lineage_wf -t 60 -x fasta dataflow/01-checkmin dataflow/02-checkmout")
-	os.system("checkm qa dataflow/02-checkmout/lineage.ms dataflow/02-checkmout > dataflow/02-checkmout/qa_out.log")
+	os.system("checkm taxonomy_wf -t 60 -x fasta dataflow/01-checkmin dataflow/02-checkmout")
+	#os.system("checkm qa dataflow/02-checkmout/lineage.ms dataflow/02-checkmout > dataflow/02-checkmout/qa_out.log")
 
 
 runcommand = input("\n" + "Clean up checkM results (also export a prevotella only table)? (y or n):")
