f9c1d9759c972f8eb2fca183e7896b30d8d26f05
CameronStrachan <strachc@gmail.com>
2019-02-26 19:02:52 +0100

    y

--- wolf3/test.py
+++
@@ -1,7 +0,0 @@
-import os, sys
-import pandas as pd
-
-df_meta = pd.read_csv('dataflow/00-meta/sample-metadata.tsv', sep = '\t')
-columns = list(df_meta)
-columns.remove('#SampleID')
-print(columns)
