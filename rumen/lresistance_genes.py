5dd48dc0fc5607eefd63c25ee645e02ca67fbc4a
CameronStrachan <strachc@gmail.com>
2019-03-29 13:12:51 +0100

    y

---
+++ rumen/lresistance_genes.py
@@ -0,0 +1,15 @@
+import os, sys
+import subprocess
+import pandas as pd
+
+# custom libraries
+sys.path.insert(0, '/home/strachan/master/')
+from modules import seq_core_lin as sc
+from modules import seq_gen_lin as sg
+
+contigs = ['4309689-submission.assembly_79', '4309680-submission.assembly_52', 'RUG782_1']
+
+file_obj = sc.Fasta('rumen_genomes.fasta', 'dataflow/01-nucl/')
+file_obj.setOutputName('rumen_genomes_resistance_genes.fasta')
+file_obj.setOutputLocation('dataflow/01-nucl/')
+file_obj.subsetfasta(seqlist = contigs, headertag='resistance_genes')
