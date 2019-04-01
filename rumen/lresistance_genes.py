3c092e96abf29ef2f6a5cdd6f573b50675db4d18
CameronStrachan <strachc@gmail.com>
2019-04-01 09:55:48 +0200

    y

--- rumen/lresistance_genes.py
+++ rumen/lresistance_genes.py
@@ -10,6 +10,8 @@ from modules import seq_gen_lin as sg
 
 # this is from the recent blast of all rumen genomes
 
+# run prodigal
+
 contigs = ['4309689-submission.assembly_79', '4309680-submission.assembly_52', 'RUG782_1']
 
 file_obj = sc.Fasta('rumen_genomes.fasta', 'dataflow/01-nucl/')
@@ -20,7 +22,7 @@ file_obj.subsetfasta(seqlist = contigs, headertag='resistance_genes')
 file_obj = sc.Fasta('rumen_genomes_resistance_genes.fasta', 'dataflow/01-nucl/')
 file_obj.setOutputName('rumen_genomes_resistance_genes.fasta')
 file_obj.setOutputLocation('dataflow/01-prot/')
-file_obj.runprodigal()
+#file_obj.runprodigal()
 
 
 file = 'resistance_island_blast_hits_concatenated.fasta'
@@ -30,7 +32,7 @@ file_obj = sc.Fasta(file, 'dataflow/01-nucl/')
 outputfilename = file.split(".f")[0] + '_extractedCONTIGs' + '.fasta'
 file_obj.setOutputName(outputfilename)
 file_obj.setOutputLocation('dataflow/01-nucl/')
-file_obj.extractORFs_gff3(gff3_table_loc = 'dataflow/00-meta/resistance_blast_hit_cotigs.csv')
+#file_obj.extractORFs_gff3(gff3_table_loc = 'dataflow/00-meta/resistance_blast_hit_cotigs.csv')
 
 indir = 'dataflow/01-nucl/'
 headerfile = 'dataflow/02-headers/'
@@ -43,3 +45,26 @@ headers = file_obj.fasta2headermap()
 df = pd.DataFrame.from_dict(headers, orient="index")
 df['file'] = file
 df.to_csv(headerfile + file.split('.fa')[0] + '.csv')
+
+# blast islands to each other
+
+file = "rumen_genomes_resistance_genes.fasta"
+indir = 'dataflow/01-prot/'
+blastdbdir = 'dataflow/02-blast-db/'
+blastdir = 'dataflow/02-blast/'
+
+file_obj = sc.Fasta(file, indir)
+file_obj.setOutputName(file)
+file_obj.setOutputLocation(blastdbdir)
+file_obj.runmakeblastdb(dbtype='nucl')
+
+file = "resistance_island_blast_hits_concatenated_extractedCONTIGs_3rumen.fasta"
+
+file_obj = sc.Fasta(file, indir)
+file_obj.setOutputLocation(blastdir)
+
+outputfilename = "resistance_island_mapping.txt"
+blastdb = "resistance_island_3rumen.fasta"
+
+file_obj.setOutputName(outputfilename)
+file_obj.runblast(blast='blastn', db=blastdb, dblocation=blastdbdir, max_target_seqs=10, evalue=1e-3, num_threads = 60)
