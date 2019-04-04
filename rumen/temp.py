069e6336c7d77ad5f84f46303c417686dbfeb20e
CameronStrachan <strachc@gmail.com>
2019-04-04 10:12:29 +0200

    y

--- rumen/temp.py
+++ rumen/temp.py
@@ -40,11 +40,12 @@ file = "rumen_genomes.fasta"
 indir = 'dataflow/01-nucl/'
 blastdbdir = 'dataflow/02-blast-db/'
 
+# blast resistance islands against all
 
 file_obj = sc.Fasta(file, indir)
 file_obj.setOutputName(file)
 file_obj.setOutputLocation(blastdbdir)
-file_obj.runmakeblastdb(dbtype='nucl')
+#file_obj.runmakeblastdb(dbtype='nucl')
 
 file = "resistance_island_blast_hits_concatenated_extractedCONTIGs_3rumen.fasta"
 blastdir = 'dataflow/02-blast/'
@@ -56,4 +57,16 @@ outputfilename = "resistance_island_blast_hits_concatenated_extractedCONTIGs_3ru
 blastdb = "rumen_genomes.fasta"
 
 file_obj.setOutputName(outputfilename)
-file_obj.runblast(blast='blastn', db=blastdb, dblocation=blastdbdir, max_target_seqs=500, evalue=1e-3, num_threads = 60, max_hsps = 10)
+#file_obj.runblast(blast='blastn', db=blastdb, dblocation=blastdbdir, max_target_seqs=500, evalue=1e-3, num_threads = 60, max_hsps = 10)
+
+
+# extract all rumen resistance islands
+
+file = 'rumen_genomes.fasta'
+
+file_obj = sc.Fasta(file, 'dataflow/01-nucl/')
+    # set output name, location
+outputfilename = file.split(".f")[0] + '_extractedCONTIGs_all_rumen' + '.fasta'
+file_obj.setOutputName(outputfilename)
+file_obj.setOutputLocation('dataflow/01-nucl/')
+file_obj.extractORFs_gff3(gff3_table_loc = 'dataflow/00-meta/resistance_blast_hit_cotigs_all_rumen.csv')
