9105687140cd6c8a2e00b5c3fb7a36c843a14d78
CameronStrachan <strachc@gmail.com>
2019-04-04 09:10:03 +0200

    y

--- rumen/temp.py
+++ rumen/temp.py
@@ -12,7 +12,7 @@ from modules import seq_gen_lin as sg
 file_obj = sc.Fasta('rumen_genomes_resistance_genes.fasta', 'dataflow/01-nucl/')
 file_obj.setOutputName('rumen_genomes_resistance_genes.fasta')
 file_obj.setOutputLocation('dataflow/01-prot/')
-file_obj.runprodigal()
+#file_obj.runprodigal()
 
 file = "resistance_island_blast_hits_concatenated_extractedCONTIGs_3rumen.fasta"
 indir = 'dataflow/01-nucl/'
@@ -22,7 +22,7 @@ blastdir = 'dataflow/02-blast/'
 file_obj = sc.Fasta(file, indir)
 file_obj.setOutputName(file)
 file_obj.setOutputLocation(blastdbdir)
-file_obj.runmakeblastdb(dbtype='nucl')
+#file_obj.runmakeblastdb(dbtype='nucl')
 
 file = "resistance_island_blast_hits_concatenated_extractedCONTIGs_3rumen.fasta"
 
@@ -31,6 +31,29 @@ file_obj.setOutputLocation(blastdir)
 
 outputfilename = "resistance_island_mapping_allvall_V2.txt"
 blastdb = "resistance_island_blast_hits_concatenated_extractedCONTIGs_3rumen.fasta"
+
+file_obj.setOutputName(outputfilename)
+#file_obj.runblast(blast='blastn', db=blastdb, dblocation=blastdbdir, max_target_seqs=500, evalue=1e-3, num_threads = 60, max_hsps = 10)
+
+
+file = "rumen_genomes.fasta"
+indir = 'dataflow/01-nucl/'
+blastdbdir = 'dataflow/02-blast-db/'
+
+
+file_obj = sc.Fasta(file, indir)
+file_obj.setOutputName(file)
+file_obj.setOutputLocation(blastdbdir)
+file_obj.runmakeblastdb(dbtype='nucl')
+
+file = "resistance_island_blast_hits_concatenated_extractedCONTIGs_3rumen.fasta"
+blastdir = 'dataflow/02-blast/'
+
+file_obj = sc.Fasta(file, indir)
+file_obj.setOutputLocation(blastdir)
+
+outputfilename = "resistance_island_blast_hits_concatenated_extractedCONTIGs_3rumen_all_genomes.txt"
+blastdb = "rumen_genomes.fasta"
 
 file_obj.setOutputName(outputfilename)
 file_obj.runblast(blast='blastn', db=blastdb, dblocation=blastdbdir, max_target_seqs=500, evalue=1e-3, num_threads = 60, max_hsps = 10)
