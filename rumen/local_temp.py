09a51dfb237c05f94646ad6a6954b697e3a9e9f5
CameronStrachan <strachc@gmail.com>
2019-04-11 15:18:49 +0200

    y

--- rumen/local_temp.py
+++ rumen/local_temp.py
@@ -9,22 +9,26 @@ sys.path.insert(0, '/Users/cameronstrachan/master/')
 from modules import seq_core_lin as sc
 from modules import seq_gen_lin as sg
 
-file_obj = sc.Fasta('aph3_ncbi.fasta', 'dataflow/01-prot/')
-file_obj.setOutputName('aph3_ncbi_250.fasta')
-file_obj.setOutputLocation('dataflow/01-prot/')
-file_obj.lengthcutoff(replaceheaders = True, length = 250, direction = 'above')
+# ant6 from rumen
 
-file_obj = sc.Fasta('aph3_ncbi_250.fasta', 'dataflow/01-prot/')
-file_obj.setOutputName('aph3_ncbi_250_350.fasta')
+file_obj = sc.Fasta('ANT6_rumen.fasta', 'dataflow/01-prot/')
+file_obj.setOutputName('ANT6_rumen_250.fasta')
 file_obj.setOutputLocation('dataflow/01-prot/')
-file_obj.lengthcutoff(replaceheaders = True, length = 350, direction = 'below')
+file_obj.lengthcutoff(replaceheaders = False, length = 250, direction = 'above')
 
-file_obj = sc.Fasta('APH3_rumen.fasta', 'dataflow/01-prot/')
-file_obj.setOutputName('APH3_rumen_250.fasta')
+file_obj = sc.Fasta('ANT6_rumen_250.fasta', 'dataflow/01-prot/')
+file_obj.setOutputName('ANT6_rumen_250_350.fasta')
 file_obj.setOutputLocation('dataflow/01-prot/')
-file_obj.lengthcutoff(replaceheaders = True, length = 250, direction = 'above')
+file_obj.lengthcutoff(replaceheaders = False, length = 350, direction = 'below')
 
-file_obj = sc.Fasta('APH3_rumen_250.fasta', 'dataflow/01-prot/')
-file_obj.setOutputName('APH3_rumen_250_350.fasta')
+# ant6 from ncbi
+
+file_obj = sc.Fasta('ANT6_ncbi.fasta', 'dataflow/01-prot/')
+file_obj.setOutputName('ANT6_ncbi_250.fasta')
 file_obj.setOutputLocation('dataflow/01-prot/')
-file_obj.lengthcutoff(replaceheaders = True, length = 350, direction = 'below')
+file_obj.lengthcutoff(replaceheaders = False, length = 250, direction = 'above')
+
+file_obj = sc.Fasta('ANT6_ncbi_250.fasta', 'dataflow/01-prot/')
+file_obj.setOutputName('ANT6_ncbi_250_350.fasta')
+file_obj.setOutputLocation('dataflow/01-prot/')
+file_obj.lengthcutoff(replaceheaders = False, length = 350, direction = 'below')
