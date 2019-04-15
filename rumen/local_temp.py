e7d5347e80adfb62c8f110eab4c8932b98ed0a45
CameronStrachan <strachc@gmail.com>
2019-04-15 12:28:02 +0200

    y

--- rumen/local_temp.py
+++ rumen/local_temp.py
@@ -81,3 +81,15 @@ file_obj = sc.Fasta('SAT4_rumen_150.fasta', 'dataflow/01-prot/')
 file_obj.setOutputName('SAT4_rumen_150_250.fasta')
 file_obj.setOutputLocation('dataflow/01-prot/')
 file_obj.lengthcutoff(replaceheaders = False, length = 250, direction = 'below')
+
+
+#../bin/muscle -in dataflow/01-prot/ANT6_ncbi_rumen_250_350_rename.fasta -out dataflow/03-alignments/ANT6_ncbi_rumen_250_350.afa -maxiters 3 -diags -sv -distance1 kbit20_3
+#../bin/FastTree dataflow/03-alignments/ANT6_ncbi_rumen_250_350.afa > dataflow/03-trees/ANT6_ncbi_rumen_250_350.afa.newick
+
+#../bin/muscle -in dataflow/01-prot/APH3_ncbi_rumen_200_300_rename.fasta -out dataflow/03-alignments/APH3_ncbi_rumen_200_300.afa -maxiters 3 -diags -sv -distance1 kbit20_3
+#../bin/FastTree dataflow/03-alignments/APH3_ncbi_rumen_200_300.afa > dataflow/03-trees/APH3_ncbi_rumen_200_300.afa.newick
+
+#../bin/muscle -in dataflow/01-prot/SAT4_ncbi_rumen_150_250_rename.fasta -out dataflow/03-alignments/SAT4_ncbi_rumen_150_250.afa -maxiters 3 -diags -sv -distance1 kbit20_3
+#../bin/FastTree dataflow/03-alignments/SAT4_ncbi_rumen_150_250.afa > dataflow/03-trees/SAT4_ncbi_rumen_150_250.afa.newick
+
+#../bin/muscle -in dataflow/01-prot/ANT6_ncbi_rumen_250_350_clade1.fasta -out dataflow/03-alignments/ANT6_ncbi_rumen_250_350_clade1.afa
