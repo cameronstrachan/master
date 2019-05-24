2eb05987ee6c0eaea4a0b5061aa8402c743caf6a
CameronStrachan <strachc@gmail.com>
2019-05-24 14:50:47 +0200

    y

--- rumen/temp2.py
+++ rumen/temp2.py
@@ -31,4 +31,11 @@ file_obj.setOutputName(outputfilename)
 
 blastdb = "JQ655275.1.fasta"
 
-file_obj.runblast(blast='blastn', db=blastdb, dblocation=blastdbdir, max_target_seqs=100, evalue=1e-3, num_threads = 60, max_hsps = 5)
+#file_obj.runblast(blast='blastn', db=blastdb, dblocation=blastdbdir, max_target_seqs=100, evalue=1e-3, num_threads = 60, max_hsps = 5)
+
+file = "JQ655275.1.fasta"
+
+file_obj = sc.Fasta(file, 'dataflow/01-nucl/')
+file_obj.setOutputName(file)
+file_obj.setOutputLocation('dataflow/01-prot/')
+file_obj.runprodigal()
