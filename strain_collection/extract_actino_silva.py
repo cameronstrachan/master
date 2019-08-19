479a4f175de34c0e366b0c3908efb9afe516c52a
CameronStrachan <strachc@gmail.com>
2019-08-19 17:49:20 +0200

    y

--- strain_collection/extract_actino_silva.py
+++ strain_collection/extract_actino_silva.py
@@ -17,9 +17,7 @@ from modules import seq_gen_lin as sg
 actino_seqs = pd.read_csv('actinobacteria_silva_seqs.csv', low_memory=False)
 actino_seqs_acc = actino_seqs['accession'].tolist()
 
-print(actino_seqs_acc)
-
-file_obj = sc.Fasta('silva_132_99_16S.fna', '00-database/')
+file_obj = sc.Fasta('silva_132_99_16S.fna', '00-databases/')
 file_obj.setOutputName('actino_bacteria_seqs.fasta')
 file_obj.setOutputLocation('01-nucl/')
 file_obj.subsetfasta(seqlist = actino_seqs_acc, headertag='actino')
