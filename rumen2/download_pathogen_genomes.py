d80198c1cb17040788cc477552f9ae61c2772452
CameronStrachan <strachc@gmail.com>
2019-09-16 08:16:14 +0200

    y

---
+++ rumen2/download_pathogen_genomes.py
@@ -0,0 +1,60 @@
+# python libraries
+import os, sys
+import subprocess
+import pandas as pd
+
+from Bio import Entrez, SeqIO
+
+Entrez.email = 'strachc@gmail.com'
+
+# custom libraries
+system = str(input('\n' + 'Local or Server (L or S):'))
+
+if system == 'S':
+    sys.path.insert(0, '/home/strachan/master/')
+else:
+    sys.path.insert(0, '/Users/cameronstrachan/master/')
+
+from modules import seq_core_lin as sc
+from modules import seq_gen_lin as sg
+
+def get_assembly_summary(id):
+    """Get esummary for an entrez id"""
+    from Bio import Entrez
+    esummary_handle = Entrez.esummary(db="assembly", id=id, report="full")
+    esummary_record = Entrez.read(esummary_handle)
+    return esummary_record
+
+def get_assemblies(term, download=True, path='assemblies'):
+    """Download genbank assemblies for a given search term.
+    Args:
+        term: search term, usually organism name
+        download: whether to download the results
+        path: folder to save to
+    """
+
+    handle = Entrez.esearch(db="assembly", term=term, retmax='200')
+    record = Entrez.read(handle)
+    ids = record['IdList']
+    print (f'found {len(ids)} ids')
+    links = []
+    for id in ids:
+        #get summary
+        summary = get_assembly_summary(id)
+        #get ftp link
+        url = summary['DocumentSummarySet']['DocumentSummary'][0]['FtpPath_RefSeq']
+        if url == '':
+            continue
+        label = os.path.basename(url)
+        #get the fasta link - change this to get other formats
+        link = os.path.join(url,label+'_genomic.fna.gz')
+        print (link)
+        links.append(link)
+        if download == True:
+            #download link
+            urllib.request.urlretrieve(link, f'{label}.fna.gz')
+    return links
+
+links = get_assemblies("Listeria monocytogenes", download=False)
+
+print(links)
