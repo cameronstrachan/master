45636885ad59f6407c344bf920f55b0f458bcb8f
CameronStrachan <strachc@gmail.com>
2019-09-18 12:02:01 +0200

    y

--- rumen2/test.py
+++ rumen2/test.py
@@ -14,6 +14,16 @@ else:
 from modules import seq_core_lin as sc
 from modules import seq_gen_lin as sg
 
+genes = []
+analysis_folder = 'dataflow_test/03-analysis/'
+for file in output_files_freq:
+    csv_file = analysis_folder + file
+    df = pd.read_csv(csv_file, low_memory=False)
+    gene_ids = df['gene'].tolist()
+    genes = genes + gene_ids
+
+genes_unique = list(set(genes))
+
 blastin = 'dataflow_test/01-nucl/'
 input_file = 'stewart2019_mags_genes_sub.fasta'
 
@@ -21,7 +31,8 @@ file_obj = sc.Fasta(input_file, blastin)
 file_obj.setOutputName(input_file)
 headers = file_obj.fasta2headermap()
 
-headerfile = 'dataflow_test/03-anlysis/'
+headerfile = 'dataflow_test/03-analysis/'
 df = pd.DataFrame.from_dict(headers, orient="index")
 df['file'] = input_file
+#df[df['A'].isin(genes_unique]
 df.to_csv(headerfile + input_file.split('.fa')[0] + '.csv')
