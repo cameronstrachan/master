30448fef79365fecf8d26093048e684183c9c2d1
CameronStrachan <strachc@gmail.com>
2019-09-18 13:08:23 +0200

    y

--- rumen2/test.py
+++ rumen2/test.py
@@ -56,8 +56,10 @@ df['file'] = input_file
 df.columns = ['id', 'full_header']
 df['id_unnumbered'] = 'NA'
 
-for index, row in df.iterrows():
-    row['id_unnumbered'] = str(row['id']).rsplit('_', 1)[0]
+#for index, row in df.iterrows():
+#    row['id_unnumbered'] = str(row['id']).rsplit('_', 1)[0]
+
+df['id_unnumbered'] = df.apply(lambda x: fxy(str(x['A']).rsplit('_', 1)[0]), axis=1)
 
 df = df[df['id_unnumbered'].isin(genes_unique)]
 df.to_csv(headerfile + input_file.split('.fa')[0] + '_headers.csv')
