664207c7a4b2265504eb8b188d58ee66e2b6ab65
CameronStrachan <strachc@gmail.com>
2019-09-19 16:13:48 +0200

    y

--- rumen2/test.py
+++ rumen2/test.py
@@ -19,10 +19,6 @@ blastxml = 'dataflow_test/02-blast-xml/'
 blastin = 'dataflow_test/01-nucl/'
 prot_dir = 'dataflow_test/01-prot/'
 
-file_obj = sc.Fasta('stewart2019_mags_genes_sub_pathogen_mapped.fasta', prot_dir)
-file_obj.setOutputLocation(blastxml)
-file_obj.runonlineblast(numhits=1)
-
-blastfiles = 'stewart2019_mags_genes_sub_pathogen_mapped.fasta'
+blastfiles = ['stewart2019_mags_genes_sub_pathogen_mapped.fasta']
 xmlfiles = [f for f in os.listdir(blastxml) if f.endswith(".xml")]
 sg.blastxmltotable(xmlinputfolder=blastxml, blastinputfolder=blastin,outputpath='dataflow/03-analysis/compiled_annotations.txt', xmlfilenames=xmlfiles, blastfilename=blastfiles)
