d0e70964821d5a5c35dea3d565a9dc777fc811b7
CameronStrachan <strachc@gmail.com>
2019-09-30 20:58:54 +0200

    y

--- rumen2/extract_recombining_clusters.py
+++ rumen2/extract_recombining_clusters.py
@@ -14,12 +14,12 @@ else:
 from modules import seq_core_lin as sc
 from modules import seq_gen_lin as sg
 
-file_obj = sc.Fasta('campylobacter_coli.fasta', 'dataflow/01-nucl')
+file_obj = sc.Fasta('campylobacter_coli.fasta', 'dataflow/01-nucl/')
 file_obj.setOutputName('campylobacter_coli_extracted_clusters.fasta')
-file_obj.setOutputLocation('dataflow/01-nucl')
+file_obj.setOutputLocation('dataflow/01-nucl/')
 file_obj.extract_regions(df = 'dataflow/03-analysis/cluster_positions_ccoli.csv', col_start = "cluster_start", col_end = "cluster_end", col_contig = "pathogen_genome_id")
 
-file_obj = sc.Fasta('campylobacter_jejuni.fasta', 'dataflow/01-nucl')
+file_obj = sc.Fasta('campylobacter_jejuni.fasta', 'dataflow/01-nucl/')
 file_obj.setOutputName('campylobacter_jejuni_extracted_clusters.fasta')
-file_obj.setOutputLocation('dataflow/01-nucl')
+file_obj.setOutputLocation('dataflow/01-nucl/')
 file_obj.extract_regions(df = 'dataflow/03-analysis/cluster_positions_cjejuni.csv', col_start = "cluster_start", col_end = "cluster_end", col_contig = "pathogen_genome_id")
