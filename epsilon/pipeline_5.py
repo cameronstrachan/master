6972eaab90fb50b21f3518b5cd2fa48c59af488c
CameronStrachan <strachc@gmail.com>
2020-03-31 13:51:49 +0200

    y

--- epsilon/pipeline_5.py
+++ epsilon/pipeline_5.py
@@ -22,7 +22,7 @@ file_obj.setOutputName('campylobacter_genomes_db')
 file_obj.setOutputLocation('dataflow/02-blast-db/')
 file_obj.runmakeblastdb()
 
-file_obj = sc.Fasta('novel_campy16s.fasta', 'dataflow/03-asv-seqs/')
+file_obj = sc.Fasta('novel_campy16s.fasta', 'dataflow/01-nucl/')
 file_obj.setOutputName('novel_campy16s_campylobacter_genomes_mapped')
 file_obj.setOutputLocation('dataflow/02-blast/')
 file_obj.runblast(max_target_seqs=100, db='campylobacter_genomes_db')
