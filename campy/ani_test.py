ed8729248b407fdd2544812e6d777b5539444ea3
CameronStrachan <strachc@gmail.com>
2020-04-20 19:42:04 +0200

    y

--- campy/ani_test.py
+++ campy/ani_test.py
@@ -9,10 +9,10 @@ files = [f for f in os.listdir('dataflow/01-nucl/selected_genomes/') if f.endswi
 
 for file in files:
   file1 = input_file + file
-  file1_name = file.split('.fn')
+  file1_name = file.split('.fn')[0]
   for file in files:
     file2 = input_file + file
-    file2_name = file.split('.fn')
+    file2_name = file.split('.fn')[0]
 
     output = output_file + file1_name + '_' + file2_name
     command = 'fastANI -t 40 -q ' + file1 + ' -r ' + file2 + ' -o ' + output
