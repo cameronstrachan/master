c5b8888dfdd506c7a9a07e95fda4cb48bd22d756
CameronStrachan <strachc@gmail.com>
2020-04-20 19:56:27 +0200

    y

--- campy/ani_test.py
+++ campy/ani_test.py
@@ -13,7 +13,7 @@ for file in files:
   file1 = input_file + file
   file1_name = file.split('.fn')[0]
   output = output_file + file1_name + '.txt'
-  command = 'fastANI -t 70 -q ' + file1 + ' --rl ' + 'dataflow/genomes_list.txt' + ' -o ' + output
+  command = 'fastANI -t 70 -q ' + file1_name + ' --rl ' + 'dataflow/genomes_list.txt' + ' -o ' + output
   os.system(command)
 
 os.remove('dataflow/genomes_list.txt')
