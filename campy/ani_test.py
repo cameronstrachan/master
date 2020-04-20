fa832575e50783782822dd0e0180d9d249769d09
CameronStrachan <strachc@gmail.com>
2020-04-20 19:40:50 +0200

    y

---
+++ campy/ani_test.py
@@ -0,0 +1,21 @@
+import os, sys
+
+input_file = 'dataflow/01-nucl/selected_genomes/'
+output_file = 'dataflow/02-ani/'
+
+files = [f for f in os.listdir('dataflow/01-nucl/selected_genomes/') if f.endswith(".fna")]
+
+'fastANI -q '
+
+for file in files:
+  file1 = input_file + file
+  file1_name = file.split('.fn')
+  for file in files"
+    file2 = input_file + file
+    file2_name = file.split('.fn')
+
+    output = output_file + file1_name + '_' + file2_name
+    command = 'fastANI -t 40 -q ' + file1 + ' -r ' + file2 + ' -o ' + output
+
+command = 'cat *.txt > compiled.txt'
+os.system(command)
