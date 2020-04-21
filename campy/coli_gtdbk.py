93cba0de609b059d916c4280603b65c1f33fc385
CameronStrachan <strachc@gmail.com>
2020-04-21 11:56:13 +0200

    y

--- campy/coli_gtdbk.py
+++ campy/coli_gtdbk.py
@@ -2,14 +2,14 @@ import os, sys
 import pandas as pd
 
 
-input_selected_genus = 'dataflow/00-meta/ani_drep_ccoli_trim.csv'
+input_selected_species = 'dataflow/00-meta/ani_drep_ccoli_trim.csv'
 df_selected_genus = pd.read_csv(input_selected_species)
 
 input_dir = 'dataflow/01-nucl/'
 output_dir = 'dataflow/01-nucl/selected_genomes_coli/'
 classification_dir = 'dataflow/02-classification/selected_genomes_coli/'
 
-for index, row in df_selected_genus_samples.iterrows():
+for index, row in input_selected_species.iterrows():
     file = row['genome1']
     input_file = input_dir + file
     command = 'cp ' + input_file + ' ' + output_dir
