702caadaf606804a18309f3f7b2cb265933c72a4
CameronStrachan <strachc@gmail.com>
2020-05-14 23:20:19 +0200

    y

--- ar/pathogens_download.py
+++ ar/pathogens_download.py
@@ -6,25 +6,25 @@ pathogens = ['mycobacterium tuberculosis', 'staphylococcus aureus', 'campylobact
 
 df_folder = 'dataflow/01-dbs/pathogens/'
 
-    for pathogen in pathogens:
+for pathogen in pathogens:
 
-        pathogen_string = '\'' + pathogen + '\''
+    pathogen_string = '\'' + pathogen + '\''
 
-        command = 'esearch -db assembly -query ' + pathogen + ' | esummary | xtract -pattern DocumentSummary -element FtpPath_GenBank'
+    command = 'esearch -db assembly -query ' + pathogen + ' | esummary | xtract -pattern DocumentSummary -element FtpPath_GenBank'
 
-        process = subprocess.Popen(command, universal_newlines=True, stdout=subprocess.PIPE, shell=True)
-        output, error = process.communicate()
+    process = subprocess.Popen(command, universal_newlines=True, stdout=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
 
-        lines = output.splitlines()
+    lines = output.splitlines()
 
-        output_folder = df_folder + pathogen.replace(' ', '_') + '/'
+    output_folder = df_folder + pathogen.replace(' ', '_') + '/'
 
-        os.mkdir(output_folder)
+    os.mkdir(output_folder)
 
-        for line in lines:
+    for line in lines:
 
-            file_name = 'GCA_' + line.split('GCA_')[1] + '_genomic.fna.gz'
-            ftp = line + '/' + file_name
-            command = 'wget ' + ftp + ' -P ' + output_folder
+        file_name = 'GCA_' + line.split('GCA_')[1] + '_genomic.fna.gz'
+        ftp = line + '/' + file_name
+        command = 'wget ' + ftp + ' -P ' + output_folder
 
-            os.system(command)
+        os.system(command)
