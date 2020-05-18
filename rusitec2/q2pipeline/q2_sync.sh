9e46dfcd7ce498f9982fc41b30c6f42cc193100f
CameronStrachan <strachc@gmail.com>
2020-05-18 21:13:36 +0200

    y

---
+++ rusitec2/q2pipeline/q2_sync.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+scp strachan@biodatascience.tulln.fhwn.ac.at:/home/strachan/master/rusitec/dataflow/02-qiime-viz/* /Users/cameronstrachan/master/rusitec/dataflow/02-qiime-viz/
+scp strachan@biodatascience.tulln.fhwn.ac.at:/home/strachan/master/rusitec/dataflow/03-asv-table/* /Users/cameronstrachan/master/rusitec/dataflow/03-asv-table/
+scp strachan@biodatascience.tulln.fhwn.ac.at:/home/strachan/master/rusitec/dataflow/03-asv-seqs/* /Users/cameronstrachan/master/rusitec/dataflow/03-asv-seqs/
