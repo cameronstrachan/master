e6da1812f17a70cb83c88df2d02af73f0e9fbdfa
CameronStrachan <strachc@gmail.com>
2020-05-18 13:43:54 +0200

    y

--- ar/CARD_hits_summarize.R
+++ ar/CARD_hits_summarize.R
@@ -3,7 +3,6 @@ library(stringr)
 library(stringi)
 
 setDTthreads(10)
-
 args = commandArgs(trailingOnly=TRUE)
 
 if (length(args)==0) {
@@ -15,7 +14,6 @@ if (length(args)==0) {
 }
 
 # combine all hits above the set cutoff for all blast outputs
-
 folder <- "~/master/ar/dataflow/03-blast/CARD/"
 files <- list.files(folder, pattern = "\\.txt$")
 
@@ -41,19 +39,27 @@ df_hit_summary <- rbindlist(df_list)
 colnames(df_hit_summary) <- c("query_id", "card_id", "percent_identity", "query_length", "alignment_length")
 
 # calcuate alignment percent
-
 df_hit_summary[, alignment_percent := (alignment_length/query_length)*100]
 
 # split query id into the gene id, contig id and genome file name
-
 for (i in 1:nrow(df_hit_summary)){
+  # split query id
   df_hit_summary[i,"gene_id"] <- stri_reverse(str_split_fixed(stri_reverse(df_hit_summary[i,"query_id"]), "_", 3)[[1]])
   df_hit_summary[i,"contig_id"] <- stri_reverse(str_split_fixed(stri_reverse(df_hit_summary[i,"query_id"]), "_", 3)[[2]])
-  df_hit_summary[i,"genome_file"] <- paste(stri_reverse(str_split_fixed(stri_reverse(df_hit_summary[i,"query_id"]), "_", 3)[[3]]), "_rename.fasta", sep = "")
+  df_hit_summary[i,"genome_id"] <- stri_reverse(str_split_fixed(stri_reverse(df_hit_summary[i,"query_id"]), "_", 3)[[3]])
+  # split CARD id
+  df_hit_summary[i,"card_accession"] <- str_split_fixed(df_hit_summary[i,"card_id"], "|", 4)[[2]]
+  df_hit_summary[i,"aro_accession"] <- str_split_fixed(df_hit_summary[i,"card_id"], "|", 4)[[3]]
+  df_hit_summary[i,"card_annotation"] <- str_split_fixed(df_hit_summary[i,"card_id"], "|", 4)[[4]]
 }
 
+# add extension for file name
+df_hit_summary$file <- paste(df_hit_summary$genome_id, "_rename.fasta", sep = "")
+
+# clean up card annotation
+df_hit_summary$card_annotation <- gsub("_$", "", df_hit_summary$card_annotation)
+df_hit_summary$card_annotation <- gsub("_", " ", df_hit_summary$card_annotation)
+
 # save the file with the cutoff applied in the file name
-
 save_file <- paste("dataflow/04-tables/CARD_hits_", as.character(cutoff), ".csv", sep = "")
-
 write.csv(df_hit_summary, save_file, row.names = FALSE)
