11d6d65f7eb9c08554cfe2bb5e0f77b29d5d78df
CameronStrachan <strachc@gmail.com>
2020-05-18 12:34:50 +0200

    y

--- ar/CARD_hits_summarize.R
+++ ar/CARD_hits_summarize.R
@@ -1,6 +1,18 @@
 library(data.table)
 setDTthreads(10)
 
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)==0) {
+ cutoff = 95
+} else if (length(args)==1) {
+  cutoff = as.numeric(args[1])
+} else {
+  stop("Only provide one argument!", call.=FALSE)
+}
+
+
+
 folder <- "~/master/ar/dataflow/03-blast/CARD/"
 files <- list.files(folder, pattern = "\\.txt$")
 
@@ -13,8 +25,8 @@ for (file in files){
   df <- read.delim(input_file, header=FALSE)
   df <- data.table(df)
   
-  if (nrow(df[V3 >= 60]) > 0){
-    df_select = df[V3 >= 60]
+  if (nrow(df[V3 >= cutoff]) > 0){
+    df_select = df[V3 >= cutoff]
     df_select = df_select[,.(V1,V2,V3,V11,V12)]
     df_list[[i]] <- df_select
     i <- i + 1
@@ -23,4 +35,7 @@ for (file in files){
 }
 
 df_complete <- rbindlist(df_list)
-write.csv(df_complete, "~/master/ar/dataflow/04-tables/CARD_hits_100.csv")
+
+save_file <- paste("CARD_hits_", as.character(cutoff), ".csv", sep = "")
+
+write.csv(df_complete, save_file)
