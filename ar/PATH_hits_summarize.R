8523db7b7105957e03e3a915876c8993f73986e7
CameronStrachan <strachc@gmail.com>
2020-05-27 13:25:06 +0200

    y

--- ar/PATH_hits_summarize.R
+++ ar/PATH_hits_summarize.R
@@ -19,8 +19,6 @@ df_hit_summary <- summarize_blast_output(folder = folder, files = files, cutoff 
 # split query id into the gene id, contig id and genome file name
 for (i in 1:nrow(df_hit_summary)){
   # split query id
-  df_hit_summary[i,"gene_id"] <- stri_reverse(str_split_fixed(stri_reverse(df_hit_summary[i,"query_id"]), "_", 3)[[1]])
-  df_hit_summary[i,"contig_id"] <- stri_reverse(str_split_fixed(stri_reverse(df_hit_summary[i,"query_id"]), "_", 3)[[2]])
   df_hit_summary[i,"genome_id"] <- stri_reverse(str_split_fixed(stri_reverse(df_hit_summary[i,"query_id"]), "_", 3)[[3]])
   # split subject id
   df_hit_summary[i,"pathogen_genome_id"] <- stri_reverse(str_split_fixed(stri_reverse(df_hit_summary[i,"subject_id"]), "_", 2)[[2]])
@@ -28,3 +26,14 @@ for (i in 1:nrow(df_hit_summary)){
 
 # add extension for file name
 df_hit_summary$pathogen_file <- paste(df_hit_summary$pathogen_genome_id, "_rename.fasta", sep = "")
+
+# merge with card meta data
+df_card_hits <- read.csv("~/master/ar/dataflow/04-tables/CARD_hits_95_90.csv")
+df_card_hits$percent_identity <- NULL
+df_card_hits$percent_alignment <- NULL
+df_card_hits$file <- NULL
+df_card_hits$query_file <- NULL
+df_card_hits$gene_id <- NULL
+df_card_hits$contig_id <- NULL
+
+df_final  <- merge(x=df_hit_summary,y=df_card_hits,by="query_id",all.x=TRUE)
