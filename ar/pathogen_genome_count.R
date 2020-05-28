905feb321f0ccd056a1528d8520a143c4416bb29
CameronStrachan <strachc@gmail.com>
2020-05-28 10:04:27 +0200

    y

---
+++ ar/pathogen_genome_count.R
@@ -0,0 +1,14 @@
+pathogens = c('staphylococcus_aureus', 'campylobacter_coli', 'campylobacter_jejuni', 'clostridioides_difficile', 'salmonella_typhimurium', 'salmonella_newport', 'staphylococcus_pseudintermedius', 'streptococcus_agalactiae', 'enterococcus_faecium', 'erysipelothrix_rhusiopathiae', 'streptococcus_suis')
+location <- "~/master/ar/dataflow/01-dbs/pathogens/"
+
+count_list <- list()
+
+for (pathogen in pathogens){
+    folder <- paste(location, pathogen, '/', sep = '')
+    n_files <- length(list.files(folder, pattern = "\\.txt$"))
+    count_list[[pathogen]] <- n_files
+}
+
+df_count <- as.data.frame(count_list)
+write.csv(df_count, '~/master/ar/dataflow/00-meta/pathogen_genome_count.csv')
+
