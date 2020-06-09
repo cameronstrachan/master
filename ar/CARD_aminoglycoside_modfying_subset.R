e5765ce015c38862deb260134bdc5f102f39298d
CameronStrachan <strachc@gmail.com>
2020-06-09 16:48:26 +0200

    y

--- ar/CARD_aminoglycoside_modfying_subset.R
+++ ar/CARD_aminoglycoside_modfying_subset.R
@@ -7,4 +7,4 @@ df_card_hits <- read_csv("~/master/ar/dataflow/04-tables/CARD_hits_95_90.csv")
 df_card_hits_aminoglycoside <- df_card_hits %>%
   filter(drug_class == "aminoglycoside antibiotic" & resistance_mechanism != "antibiotic efflux")
 
-write.csv("~/master/ar/dataflow/04-tables/CARD_hits_30_60_aminoglycoside_moedifying.csv")
+write.csv(df_card_hits_aminoglycoside, "~/master/ar/dataflow/04-tables/CARD_hits_30_60_aminoglycoside_moedifying.csv")
