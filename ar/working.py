e0ed11b4e3ef0fd107c1441a4de476346e332ef1
CameronStrachan <strachc@gmail.com>
2020-06-10 09:58:04 +0200

    y

--- ar/working.py
+++ ar/working.py
@@ -17,36 +17,37 @@ from modules import seq_core as sc
 input_dir_genes  = 'dataflow/02-genes/'
 input_dir_prots = 'dataflow/02-prots/'
 
-output_dir_genes = 'dataflow/03-selected-genes/aminoglycoside_modifying/'
-output_dir_prots = 'dataflow/03-selected-prots/aminoglycoside_modifying/'
+output_dir_genes_aph = 'dataflow/03-selected-genes/aminoglycoside_modifying/phosphotransferases/'
+output_dir_prots_aph = 'dataflow/03-selected-prots/aminoglycoside_modifying/phosphotransferases/'
 
+output_dir_genes_ant = 'dataflow/03-selected-genes/aminoglycoside_modifying/nucleotidyltransferases/'
+output_dir_prots_ant = 'dataflow/03-selected-prots/aminoglycoside_modifying/nucleotidyltransferases/'
 
 # aminoglycoside phosphotransferases
 
 df_ac_nt_selected_phosphotransferase = pd.read_csv('dataflow/04-tables/CARD_hits_30_60_aminoglycoside_modifying_aph.csv', low_memory=False)
-files1 = list(set(df_ac_nt_selected_phosphotransferase['query_id'].tolist()))
 
-for file in files1:
+for index, row in df_ac_nt_selected_phosphotransferase.iterrows():
 
-    file_w_ext = file + '.fasta'
+    orf = row['query_id']
+    file = row['file']
 
-    command = 'cp ' + input_dir_genes + file_w_ext + ' ' + output_dir_genes + 'phosphotransferases/'
-    os.system(command)
+    file_obj = sc.Fasta(file, input_dir_genes)
+    file_obj.setOutputName(orf + '.fasta')
+    file_obj.setOutputLocation(output_dir_genes_aph)
+    file_obj.subsetfasta(seqlist = [orf])
 
-    command = 'cp ' + input_dir_prots + file_w_ext + ' ' + output_dir_prots + 'phosphotransferases/'
-    os.system(command)
 
 # aminoglycoside nucleotidyltransferases
 
 df_ac_nt_selected_nucleotidyltransferase = pd.read_csv('dataflow/04-tables/CARD_hits_30_60_aminoglycoside_modifying_ant.csv', low_memory=False)
-files2 = list(set(df_ac_nt_selected_nucleotidyltransferase['query_id'].tolist()))
 
-for file in files2:
+for index, row in df_ac_nt_selected_phosphotransferase.iterrows():
 
-    file_w_ext = file + '.fasta'
+    orf = row['query_id']
+    file = row['file']
 
-    command = 'cp ' + input_dir_genes + file_w_ext + ' ' + output_dir_genes + 'nucleotidyltransferases/'
-    os.system(command)
-
-    command = 'cp ' + input_dir_prots + file_w_ext + ' ' + output_dir_prots + 'nucleotidyltransferases/'
-    os.system(command)
+    file_obj = sc.Fasta(file, input_dir_genes)
+    file_obj.setOutputName(orf + '.fasta')
+    file_obj.setOutputLocation(output_dir_genes_ant)
+    file_obj.subsetfasta(seqlist = [orf])
