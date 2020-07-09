14245306fad4846ab5e8b1075ab67b651ee0e139
CameronStrachan <strachc@gmail.com>
2020-07-09 12:23:14 +0200

    y

--- chyo/CARD_blast.py
+++ chyo/CARD_blast.py
@@ -18,9 +18,9 @@ from modules import seq_gen_lin as sg
 file_obj = sc.Fasta('card_db.fasta', 'fasta/')
 file_obj.setOutputName('card_db.fasta')
 file_obj.setOutputLocation('blastdb/')
-file_obj.runmakeblastdb(dbtype='nucl')
+file_obj.runmakeblastdb(dbtype='prot')
 
 file_obj = sc.Fasta('chyo_transcriptome_concensus_genome_prot_trimmed_100.fasta', 'fasta/')
 file_obj.setOutputLocation('blast_output/')
 file_obj.setOutputName('CARD_blast.txt')
-file_obj.runblast(blast='blastn', db='card_db.fasta', dblocation='blastdb/', max_target_seqs=1, evalue=1e-5, num_threads = 30)
+file_obj.runblast(blast='blastp', db='card_db.fasta', dblocation='blastdb/', max_target_seqs=1, evalue=1e-5, num_threads = 30)
