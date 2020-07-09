5d49d4973f74ac7f0d065422a922eb0b3e7e0601
CameronStrachan <strachc@gmail.com>
2020-07-09 14:20:59 +0200

    y

--- chyo/CARD_blast.py
+++ chyo/CARD_blast.py
@@ -22,5 +22,5 @@ from modules import seq_gen_lin as sg
 
 file_obj = sc.Fasta('cyo1_nanopore.fasta', 'fasta/')
 file_obj.setOutputLocation('blast_output/')
-file_obj.setOutputName('ccoli_hgt.txt')
-file_obj.runblast(blast='blastn', db='campylobacter_coli', dblocation='blastdb/', max_target_seqs=1000, evalue=1e-5, num_threads = 30)
+file_obj.setOutputName('cjejuni_hgt.txt')
+file_obj.runblast(blast='blastn', db='campylobacter_jejuni', dblocation='blastdb/', max_target_seqs=1000, evalue=1e-5, num_threads = 30)
