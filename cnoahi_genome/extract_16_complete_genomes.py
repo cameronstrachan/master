7ae5e8a9f6dae66effdff43389f07cb356ab9aa9
CameronStrachan <strachc@gmail.com>
2020-08-06 12:10:28 +0200

    y

---
+++ cnoahi_genome/extract_16_complete_genomes.py
@@ -0,0 +1,24 @@
+import os, sys
+import subprocess
+import pandas as pd
+
+# custom libraries
+system = str(input('\n' + 'Local or Server (L or S):'))
+
+if system == 'S':
+    sys.path.insert(0, '/home/strachan/master/')
+else:
+    sys.path.insert(0, '/Users/cameronstrachan/master/')
+
+from modules import seq_core_lin as sc
+
+genome_dir = '../cnoahi_phylogeny/concatenated_marker/complete_genomes/'
+
+files = [f for f in os.listdir(genome_dir) if f.endswith(".fna")]
+outdir = 'related_genomes/prokka/'
+
+for file in files:
+
+    file_prefix = file.split(".f")[0]
+    command = 'prokka --outdir ' + outdir + ' --prefix ' + file_prefix + ' ' + genome_dir + file
+    os.system(command)
