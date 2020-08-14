1722728988244b325dd103f722cfa614b5a88c8c
CameronStrachan <strachc@gmail.com>
2020-08-14 17:22:32 +0200

    y

--- rusitec_metagenomes/working.py
+++ rusitec_metagenomes/working.py
@@ -14,7 +14,7 @@ else:
 from modules import seq_core_lin as sc
 from modules import seq_gen_lin as sg
 
-files = [f for f in os.listdir('.') if f.endswith(".fastq")]
+files = [f for f in os.listdir('dataflow/01-bam/') if f.endswith(".bam")]
 
 for file in files:
     file_id = file.split('_')[1]
@@ -22,7 +22,8 @@ for file in files:
     file_sorted = file_prefix + ".sorted.bam"
     fastq_sorted = file_prefix + ".sorted.fastq"
 
-    command = 'samtools sort --threads 60 -n dataflow/01-bam/' + file + ' -o ' + file_sorted
+    command = 'samtools sort --threads 60 -n dataflow/01-bam/' + file + ' -o dataflow/01-bam/' + file_sorted
     os.system(command)
 
-    command = 'bedtools bamtofastq -i dataflow/bam/' + file_sorted + ' -fq dataflow/01-fastq/' + file_id + '_R1.fastq' + ' -fq2 dataflow/01-fastq/' + file_id + '_R2.fastq'
+    command = 'bedtools bamtofastq -i dataflow/01-bam/' + file_sorted + ' -fq dataflow/01-fastq/' + file_id + '_R1.fastq' + ' -fq2 dataflow/01-fastq/' + file_id + '_R2.fastq'
+    os.system(command)
