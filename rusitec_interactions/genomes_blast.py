59eb906f298a005510df6f6f445f2e9d13dec2cb
CameronStrachan <strachc@gmail.com>
2020-08-17 20:39:50 +0200

    y

---
+++ rusitec_interactions/genomes_blast.py
@@ -0,0 +1,15 @@
+import os, sys
+import subprocess
+import pandas as pd
+
+# custom libraries
+system = str(input('\n' + 'Local or Server (L or S):'))
+
+if system == 'S':
+    sys.path.insert(0, '/home/strachan/master/')
+else:
+    sys.path.insert(0, '/Users/cameronstrachan/master/')
+
+from modules import seq_core_lin as sc
+
+files = [f for f in os.listdir('dataflow/01-nucl/') if f.endswith(".fasta")]
