e45be2248e2ea3c7c1e5dd6a667d7fe6f9f1c9b8
CameronStrachan <strachc@gmail.com>
2020-08-17 20:47:41 +0200

    y

--- rusitec_interactions/genomes_blast.py
+++ rusitec_interactions/genomes_blast.py
@@ -13,3 +13,26 @@ else:
 from modules import seq_core_lin as sc
 
 files = [f for f in os.listdir('dataflow/01-nucl/') if f.endswith(".fasta")]
+
+for file in files:
+   file_obj = sc.Fasta(file, 'dataflow/01-nucl/')
+   outputfilename = file.split(".f")[0] + '.faa'
+   file_obj.setOutputName(outputfilename)
+   file_obj.setOutputLocation('dataflow/01-prot/')
+   file_obj.runprodigal()
+
+for file in files:
+    file_obj = sc.Fasta(file, 'dataflow/01-prot/')
+    file_obj.setOutputName(file)
+    file_obj.setOutputLocation('dataflow/02-blastdbs/')
+    file_obj.runmakeblastdb(dbtype='prot')
+
+file = 'cnoahi_unicycler_closed.faa'
+blastdbs = files.copy()
+
+file_obj = sc.Fasta(file, 'dataflow/01-prot/')
+file_obj.setOutputLocation('dataflow/03-blastout/')
+for blastdb in blastdbs:
+	outputfilename = file.split('.f')[0] + ':' + blastdb.split('.f')[0] + '.txt'
+	file_obj.setOutputName(outputfilename)
+	file_obj.runblast(blast='blastp', db=blastdb, dblocation='dataflow/02-blastdbs/', max_target_seqs=1, evalue=1e-3, num_threads = 60)
