33b6b18b334bcabe6398c5512184196f9aead2df
CameronStrachan <strachc@gmail.com>
2020-08-24 14:34:30 +0200

    y

---
+++ rusitec_metagenomes/selected_bins.py
@@ -0,0 +1,21 @@
+import os, sys
+import subprocess
+import pandas as pd
+
+# custom libraries
+system = str(input('\n' + 'Local or Server (L or S):'))
+
+if system == 'S':
+    sys.path.insert(0, '/home/strachan/master/')
+else:
+    sys.path.insert(0, '/Users/cameronstrachan/master/')
+
+
+df_selected_bins = pd.read_csv('dataflow/00-meta/checkM_select_90_10.csv', low_memory=False)
+bins = df_representative_genomes['bin'].tolist()
+
+files = [item + ".fa" for item in bins]
+
+for file in files:
+    command = 'cp dataflow/04-bins/' + file + ' dataflow/04-bins/selected/'
+    os.system(command)
