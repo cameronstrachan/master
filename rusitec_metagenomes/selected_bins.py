c2f5e49595e3a5f7517fb82755d42b4605e20272
CameronStrachan <strachc@gmail.com>
2020-09-06 14:24:04 +0200

    y

--- rusitec_metagenomes/selected_bins.py
+++ rusitec_metagenomes/selected_bins.py
@@ -11,11 +11,11 @@ else:
     sys.path.insert(0, '/Users/cameronstrachan/master/')
 
 
-df_selected_bins = pd.read_csv('dataflow/00-meta/checkM_select_90_10.csv', low_memory=False)
+df_selected_bins = pd.read_csv('dataflow/00-meta/checkM_hybrid_select_90_10.csv', low_memory=False)
 bins = df_selected_bins['bin'].tolist()
 
 files = [item + ".fa" for item in bins]
 
 for file in files:
-    command = 'cp dataflow/04-bins/' + file + ' dataflow/04-bins/selected/'
+    command = 'cp dataflow/04-bins/hybrid/' + file + ' dataflow/04-bins/selected/'
     os.system(command)
