1541fa6c5eb55979c57891a758607ccb81947aa1
CameronStrachan <strachc@gmail.com>
2020-09-15 13:08:20 +0200

    y

--- rusitec_interactions/genomes_blast.py
+++ rusitec_interactions/genomes_blast.py
@@ -29,12 +29,14 @@ for file in files:
     file_obj.setOutputLocation('dataflow/02-blastdbs/')
     file_obj.runmakeblastdb(dbtype='prot')
 
-file = 'cnoahi_unicycler_closed.faa'
 blastdbs = files.copy()
 
-file_obj = sc.Fasta(file, 'dataflow/01-prot/')
-file_obj.setOutputLocation('dataflow/03-blastout/')
-for blastdb in blastdbs:
-	outputfilename = file.split('.f')[0] + ':' + blastdb.split('.f')[0] + '.txt'
-	file_obj.setOutputName(outputfilename)
-	file_obj.runblast(blast='blastp', db=blastdb, dblocation='dataflow/02-blastdbs/', max_target_seqs=1, evalue=1e-3, num_threads = 60)
+for file in files:
+
+    blastdbs = files.copy()
+    file_obj = sc.Fasta(file, 'dataflow/01-prot/')
+    file_obj.setOutputLocation('dataflow/03-blastout/')
+    for blastdb in blastdbs:
+    	outputfilename = file.split('.f')[0] + ':' + blastdb.split('.f')[0] + '.txt'
+    	file_obj.setOutputName(outputfilename)
+    	file_obj.runblast(blast='blastp', db=blastdb, dblocation='dataflow/02-blastdbs/', max_target_seqs=1, evalue=1e-3, num_threads = 60)
