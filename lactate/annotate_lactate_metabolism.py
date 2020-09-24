368750dac91a556dd9673c153db27919126f4655
CameronStrachan <strachc@gmail.com>
2020-09-24 09:36:10 +0200

    y

--- lactate/annotate_lactate_metabolism.py
+++ lactate/annotate_lactate_metabolism.py
@@ -51,22 +51,30 @@ for file in hitfiles:
     df_file = pd.read_csv(file_loc, sep = '\t', low_memory=False)
     orfs = list(set(df_file['qseqid'].tolist()))
 
+    print(genome_file)
+    print(orfs)
+
+
     category = file.split(':')[1].split('.txt')[0]
     subset_file = genome_file.split('.fa')[0] + ':' + category + '.fasta'
+
+    print(category)
+    print(subset_file)
+
     file_obj = sc.Fasta(genome_file, 'dataflow/01-prot/')
     file_obj.setOutputName(subset_file)
     file_obj.setOutputLocation('dataflow/01-prot/selected/')
     file_obj.subsetfasta(seqlist=orfs, headertag=category)
 
-    file_obj = sc.Fasta(genome_file, 'dataflow/01-prot/')
-    file_obj.setOutputName(genome_file)
-    headers = file_obj.fasta2headermap()
-    df = pd.DataFrame.from_dict(headers, orient="index")
-    df['file'] = genome_file
-    df['category'] = category
+    #file_obj = sc.Fasta(genome_file, 'dataflow/01-prot/')
+    #file_obj.setOutputName(genome_file)
+    #headers = file_obj.fasta2headermap()
+    #df = pd.DataFrame.from_dict(headers, orient="index")
+    #df['file'] = genome_file
+    #df['category'] = category
 
-    df = df[df['0'].isin(orfs)]
-    df_list.append(df)
+    #df = df[df['0'].isin(orfs)]
+    #df_list.append(df)
 
-df_headers = pd.concat(df_list)
-df_headers.to_csv('dataflow/00-meta/selected_prot_headers.csv')
+#df_headers = pd.concat(df_list)
+#df_headers.to_csv('dataflow/00-meta/selected_prot_headers.csv')
