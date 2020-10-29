b3cbbf3b10f32d3489879354fe42ea1ab08b2a47
CameronStrachan <strachc@gmail.com>
2020-10-29 11:07:26 +0100

    y

--- lactate/compile_lactate_annotations.R
+++ lactate/compile_lactate_annotations.R
@@ -17,7 +17,7 @@ colnames(compiled_blast_hits)[1:13] <- c("qseqid", "sseqid", "blast_pident", "ss
 
 compiled_blast_hits <- compiled_blast_hits %>%
   mutate(blast_per_aln = (length / qlen) *100) %>%
-  select(qseqid, sseqid, blast_pident, blast_per_aln, file) %>%
+  select(qseqid, sseqid, blast_pident, blast_per_aln, bitscore, file) %>%
   separate(file, into = c("genome", "category"), sep = ":")
 
 colnames(compiled_blast_hits)[1:2] <- c("gene_id", "characterized_protein")
@@ -30,27 +30,29 @@ df_list <-lapply(files,function(file){
   if(inherits(x, "try-error"))
     return(NULL)
   else
-    df$file <- gsub(".txt", "", file)
-  return(x)
+    x$file <- file
+    return(x)
 })
 
 compiled_hmm_hits <- bind_rows(df_list) %>%
-  separate(V1, into = c("hmm_domain", "hmm_pfam", "gene_id", "rm", "hmm_evalue"), sep = "\\s+") %>%
-  select(-rm)
+  separate(V1, into = c("hmm_domain", "hmm_pfam", "gene_id", "rm0", "hmm_evalue"), sep = "\\s+") %>%
+  separate(file, into = c("genome", "rm1", "rm2"), sep = ":") %>%
+  select(-rm0, -rm1, -rm2)
 
 # compile genome location from prodigal headers
 compiled_headers <- read.csv("dataflow/00-meta/selected_prot_headers.csv")
+compiled_headers$genome <- gsub(".fa", "", compiled_headers$file)
 
 compiled_headers <- compiled_headers %>%
   select(-X, -index) %>%
   separate(X0, into = c("rm", "start", "stop", "direction"), sep = " # ") %>%
-  select(-rm, -category) %>%
+  select(-rm, -category, -file) %>%
   distinct()
 
 # compile everything and save
 compiled <- full_join(compiled_headers, compiled_blast_hits) %>%
   full_join(compiled_hmm_hits) %>%
-  select(genome, gene_id, start, stop, direction, characterized_protein, blast_pident, blast_per_aln, hmm_domain, hmm_evalue) %>%
+  select(genome, gene_id, start, stop, direction, characterized_protein, blast_pident, blast_per_aln, bitscore, hmm_domain, hmm_evalue) %>%
   distinct()
 
 write.csv(compiled, 'dataflow/04-analysis-tables/compiled_lactate_annotations.csv', row.names = FALSE)
