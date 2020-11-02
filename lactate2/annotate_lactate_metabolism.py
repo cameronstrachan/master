706365e4550cb6d836412f4a01f7c6aa35b584b1
CameronStrachan <strachc@gmail.com>
2020-11-02 12:07:40 +0100

    y

--- lactate2/annotate_lactate_metabolism.py
+++ lactate2/annotate_lactate_metabolism.py
@@ -67,18 +67,18 @@ for file in hitfiles:
     df = df.rename_axis("gene_id").reset_index()
     df['file'] = genome_file
     
-    df_list2.append(df)
+    #df_list2.append(df)
 
-    df = df[df["gene_id"].isin(orfs)]
-    df_list.append(df)
+    #df = df[df["gene_id"].isin(orfs)]
+    #df_list.append(df)
     
-df_headers = pd.concat(df_list)
-df_headers.reset_index(inplace=True)
-df_headers.to_csv('dataflow/00-meta/selected_prot_headers.csv')
+#df_headers = pd.concat(df_list)
+#df_headers.reset_index(inplace=True)
+#df_headers.to_csv('dataflow/00-meta/selected_prot_headers.csv')
 
-df_headers_complete = pd.concat(df_list2)
-df_headers_complete.reset_index(inplace=True)
-df_headers_complete.to_csv('dataflow/00-meta/all_prot_headers.csv')
+#df_headers_complete = pd.concat(df_list2)
+#df_headers_complete.reset_index(inplace=True)
+#df_headers_complete.to_csv('dataflow/00-meta/all_prot_headers.csv')
 
 
 files = [f for f in os.listdir('dataflow/01-prot/selected/') if f.endswith(".fa")]
@@ -90,7 +90,7 @@ for hmm in hmms:
     for file in files:
         out_file = file.split('.fa')[0] + ':' + hmm.split('.hm')[0] + '.txt'
         command2 = 'hmmscan --tblout dataflow/03-hmmout/' + out_file + ' --cpu 60 -E 1e-3 dataflow/01-hmm/' + hmm + ' dataflow/01-prot/selected/' + file
-        os.system(command2)
+        #os.system(command2)
         
 files = ['characterized_lactate_permease.fa', 'characterized_lactate_production.fa', 'characterized_lactate_utilization.fa']
 
